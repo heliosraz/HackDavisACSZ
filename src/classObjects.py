047f9095d31e3eda495cfe9b05fd1ede18664dbd
Sunny Zhou <sutzhou@ucdavis.edu>
2022-04-17 02:54:16 -0700

    schedule class objects

---
+++ src/classObjects.py
@@ -0,0 +1,37 @@
+#Class Class
+
+from typing import List
+class Class:
+
+#string: str
+#int: int
+#float: float
+
+    # type hinting: variableName: type
+    def __init__(self, name: str, numbUnits: int, preReqs: List["Class"]) -> None:
+        self.name = name
+        self.numbUnits = numbUnits
+        self.preReqs = preReqs
+
+    @staticmethod
+    def createClass(name: str, numbUnits: int, preReqs: List["Class"]) -> "Class":
+        ...
+    '''
+    # puts prereqs in order by name, and then from largest number to smallest number
+    @staticmethod
+    def reqOrder(preReqs: List["Class"]) -> List["Class"]:
+        # puts prereqs in order by name
+        preReqs
+
+        # puts prereqs
+    '''
+
+    def printClassName(self) -> None:
+        print(f"{self.name}\n{self.numbUnits}\n")
+        for x in self.preReqs:
+            className = self.preReqs[x].name
+            print(className)
+
+            #organize by number
+
+
