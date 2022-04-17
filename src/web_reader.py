5e1da7793feacb24a01b6af54374f3e7d663f47b
Sunny Zhou <sutzhou@ucdavis.edu>
2022-04-16 23:08:21 -0700

    updated scraper to scrape entire catalog

--- src/web_reader.py
+++ src/web_reader.py
@@ -1,35 +1,36 @@
 import requests
 from bs4 import BeautifulSoup
 # import classentry
-
+url = "https://ucdavis.pubs.curricunet.com/Catalog/degrees"
+page = requests.get(url)
 class_names=[]
 class_prereqs=[]
+major_hrefs=[]
+# find page content
+soup = BeautifulSoup(page.content, "html.parser")
+# get all major urls
+block=soup.find_all("div", class_="page-content-section-wrapper")
+links=block[0].find_all("a")
 
+for link in links:
+    href=link.get("data-urlalias")
+    major_hrefs.append(str(href))
 
-class CourseWebReader:
-    def __init__(self, url: str):
-        self.url = url
-        self.page = requests.get(URL)
-        # find page content
-        soup = BeautifulSoup(self.page.content, "html.parser")
+base_url = "https://ucdavis.pubs.curricunet.com/Catalog/"
+
+for href in major_hrefs:
+    if "graduate" not in href:
+        major_url=base_url+str(href)
+        print(major_url)
+        major_page = requests.get(major_url)
+        major_soup = BeautifulSoup(major_page.content, "html.parser")
         # find all specific class blocks
-        self.class_entryinfos = soup.find_all("div", class_="container-fluid course-summary-wrapper")
+        class_entryinfos = major_soup.find_all("div", class_="container-fluid course-summary-wrapper")
         # initializing class name list
-        self.class_names=[]
-        get_classnames()
-
-# getting class name data
-    def get_classnames():
-        for classentryinfo in self.class_entryinfos:
+        for classentryinfo in class_entryinfos:
             class_temp=classentryinfo.find("div", class_="col-xs-10 col-sm-10 col-md-10 text-left left-column")
             class_names.append(class_temp.text)
-
-
-
-
-
-
-
-# for classentryinfo in classentryinfos:
-#
-#     print(classentryinfo, end="\n"*2)
+        class_data=open('class_name_data.csv', 'w')
+        for class_name in class_names:
+            class_data.write(class_name+"\n")
+class_data.close()
--- test/test_web_reader.py
+++ test/test_web_reader.py
@@ -1,9 +1,11 @@
 import unittest
 import sys
+from src import web_reader
 
 class TestWebReader(unittest.TestCase):
-    def test_create():
-
+    def test_create(url: str):
+        new_web_reader=web_reader.CourseWebReader(url)
+        new_web_reader.print_classnames()
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(sys.argv[1])
